44f1e16ff834bdd559bbabcfc14151106140bf5d
LucasFernandoChannel <131505597+LucasFernandoChannel@users.noreply.github.com>
2023-06-10 23:26:01 -0300

    Add #5 project
    Add #5 project - Control LEDs with push-ups buttons.

---
+++ arduino-code-1-to-50/5-control-leds-with-push-ups-buttons.ino
@@ -0,0 +1,72 @@
+int redLed = 2;
+int blueLed = 3;
+int greenLed = 4;
+int yellowLed = 5;
+
+int redButton = 6;
+int blueButton = 7;
+int greenButton = 8;
+int yellowButton = 9;
+
+int ledsState[4] = {0, 0, 0, 0};
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+
+  pinMode(redButton, INPUT_PULLUP);
+  pinMode(blueButton, INPUT_PULLUP);
+  pinMode(greenButton, INPUT_PULLUP);
+  pinMode(yellowButton, INPUT_PULLUP);
+
+  digitalWrite(redLed, LOW);
+  digitalWrite(blueLed, LOW);
+  digitalWrite(greenLed, LOW);
+  digitalWrite(yellowLed, LOW);
+}
+
+void loop() {
+  int redValue = digitalRead(redButton);
+  int blueValue = digitalRead(blueButton);
+  int greenValue = digitalRead(greenButton);
+  int yellowValue = digitalRead(yellowButton);
+
+  if (redValue == 0) {
+    if (ledsState[0] == 0) {
+      ledsState[0] = 1;
+    } else {
+      ledsState[0] = 0;
+    }
+  }
+
+  if (blueValue == 0) {
+    if (ledsState[1] == 0) {
+      ledsState[1] = 1;
+    } else {
+      ledsState[1] = 0;
+    }
+  }
+
+  if (greenValue == 0) {
+    if (ledsState[2] == 0) {
+      ledsState[2] = 1;
+    } else {
+      ledsState[2] = 0;
+    }
+  }      
+
+  if (yellowValue == 0) {
+    if (ledsState[3] == 0) {
+      ledsState[3] = 1;
+    } else {
+      ledsState[3] = 0;
+    }
+  }
+
+  digitalWrite(redLed, ledsState[0]);
+  digitalWrite(blueLed, ledsState[1]);
+  digitalWrite(greenLed, ledsState[2]);
+  digitalWrite(yellowLed, ledsState[3]);
+}
